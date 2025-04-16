5ec605c9ad3507fef5c4e04e5d096dcc5b176b53
Peer Sommerlund <peer.sommerlund@gmail.com>
2025-04-16 07:53:09 +0200

    Remove CommitList::marked() to avoid exposing internal structure

--- src/components/commitlist.rs
+++ src/components/commitlist.rs
@@ -174,15 +174,6 @@ impl CommitList {
 	}
 
 	///
-	#[expect(
-		clippy::missing_const_for_fn,
-		reason = "as of 1.86.0 clippy wants this to be const even though that breaks"
-	)]
-	pub fn marked(&self) -> &[(usize, CommitId)] {
-		&self.marked
-	}
-
-	///
 	pub fn clear_marked(&mut self) {
 		self.marked.clear();
 	}
